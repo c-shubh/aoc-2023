68ca1e2322901ade0c315424bc0295c7403350c1
Shubh A Chudasama <shubh.csma@gmail.com>
2023-12-04 01:09:49 +0530

    Solve Day 3 Part 2

--- Day3Part2.java
+++ Day3Part2.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Scanner;
 
-public class Day3Part1 {
+public class Day3Part2 {
     static HashSet<String> usedNumbers = new HashSet<>();
 
     public static boolean isOutOfBounds(ArrayList<?> l, int idx) {
@@ -17,7 +17,7 @@ public class Day3Part1 {
         if (!isOutOfBounds(lines, y)
                 && !isOutOfBounds(lines.get(y), x)
                 && !Character.isDigit(lines.get(y).charAt(x)))
-            return 0;
+            return -1;
         String line = lines.get(y);
         int i = x, j = x;
         while (i >= 0 && Character.isDigit(line.charAt(i))) {
@@ -35,7 +35,7 @@ public class Day3Part1 {
                 String.valueOf(j),
         });
         if (usedNumbers.contains(numberLocation)) {
-            return 0;
+            return -1;
         }
         usedNumbers.add(numberLocation);
         int num = Integer.parseInt(line.substring(i, j));
@@ -55,26 +55,45 @@ public class Day3Part1 {
             String line = lines.get(i);
             for (int j = 0; j < line.length(); j++) {
                 char curr = line.charAt(j);
-
                 // find symbol
-                if (curr != '.' && !Character.isLetterOrDigit(curr)) {
+                if (curr == '*') {
+                    int numAdj = 0;
                     // check 8 places for digit
-                    // up
-                    total += extractPartNumber(lines, j, i - 1);
-                    // down
-                    total += extractPartNumber(lines, j, i + 1);
-                    // left
-                    total += extractPartNumber(lines, j - 1, i);
-                    // right
-                    total += extractPartNumber(lines, j + 1, i);
-                    // top left
-                    total += extractPartNumber(lines, j - 1, i - 1);
-                    // top right
-                    total += extractPartNumber(lines, j + 1, i - 1);
-                    // bottom left
-                    total += extractPartNumber(lines, j - 1, i + 1);
-                    // bottom right
-                    total += extractPartNumber(lines, j + 1, i + 1);
+                    int[][] coords = new int[][] {
+                            // up
+                            { j, i - 1 },
+                            // down
+                            { j, i + 1 },
+                            // left
+                            { j - 1, i },
+                            // right
+                            { j + 1, i },
+                            // top left
+                            { j - 1, i - 1 },
+                            // top right
+                            { j + 1, i - 1 },
+                            // bottom left
+                            { j - 1, i + 1 },
+                            // bottom right
+                            { j + 1, i + 1 } };
+                    int[] res = new int[coords.length];
+
+                    for (int k = 0; k < coords.length; k++) {
+                        res[k] = extractPartNumber(lines, coords[k][0], coords[k][1]);
+                        if (res[k] != -1) {
+                            numAdj++;
+                        }
+                    }
+
+                    // curr is a gear
+                    if (numAdj == 2) {
+                        int gearRatio = 1;
+                        for (int partNo : res) {
+                            if (partNo != -1)
+                                gearRatio *= partNo;
+                        }
+                        total += gearRatio;
+                    }
                 }
             }
         }
